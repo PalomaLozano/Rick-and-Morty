6d09f2c636e035c5d6c2e20e6573fb50b4c9716c
Paloma Lozano <palomalogu9@gmail.com>
2021-10-01 19:00:05 +0200

    added html layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import '../styles/App.css';
-
+import '../styles/App.scss';
+import logoRaM from '../images/RaM.png';
 function App() {
   return (
     <div>
-      <h1>Hola mundo hi</h1>
+      <header className="header">
+        <img
+          src={logoRaM}
+          alt="rick and morty logo"
+          title="logo"
+          className="header__logo"
+        />
+      </header>
+      <main>
+        <form action="" className="form">
+          <label className="form__inputlabel">Wubba lubba lub lub</label>
+          <input
+            type="text"
+            name="name"
+            id="search"
+            className="form__inputform"
+          />
+        </form>
+        <ul>
+          <li>
+            <img src="" alt=""></img>
+            <p>Nombre</p>
+            <p>Especie</p>
+            <p>Status</p>
+          </li>
+        </ul>
+      </main>
+      <footer>This is the footer</footer>
     </div>
   );
 }
